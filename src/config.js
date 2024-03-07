6b33e58c8e8034ffc9719511b1a84369b558d683
Vasanth macbook air <s.vasanthrojin@gmail.com>
2024-03-07 09:21:16 +0800

    secure interface

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const API_URL= 'http://192.168.1.75:4545'
-export const API_URL= 'https://clicmfapi.azurewebsites.net/'
+export const API_URL= 'https://clicmfapi.azurewebsites.net'
 //export const API_URL='http://localhost:5000'
